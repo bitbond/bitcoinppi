003cf165149f97ccefa128de685ed05376907e26
trapeza <radoslav.albrecht@live.com>
2015-11-13 15:29:17 +0000

    adjusted chart labels

--- public/ppi_chart.js
+++ public/ppi_chart.js
@@ -91,10 +91,10 @@ var ppiChart = (function() {
   };
 
   var globalPPI = JSON.parse(document.getElementById("data").innerHTML);
-  setData("global_ppi", "Global PPI", globalPPI);
+  setData("global_ppi", "Global bitcoinppi", globalPPI);
 
   var selectionList = JSON.parse(document.getElementById("countryNames").innerHTML);
-  var labels = [{key: "global_ppi", label: "Global PPI"}].concat(selectionList);
+  var labels = [{key: "global_ppi", label: "Global bitcoinppi"}].concat(selectionList);
 
   // Other elements
   var xAxisGroup = svg.append("g")
@@ -188,7 +188,7 @@ var ppiChart = (function() {
       .remove();
 
     // Country select
-    var countriesForSelect = [{key: "none", label: "Select Country"}].concat(selectionList);
+    var countriesForSelect = [{key: "none", label: "Add local bitcoinppi"}].concat(selectionList);
     var countryOption = countrySelect.selectAll("option").data(countriesForSelect, function(d) { return d.key; });
 
     countryOption.enter()
